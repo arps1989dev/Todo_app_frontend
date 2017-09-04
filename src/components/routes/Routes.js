3ed2788a4092dc05e8fc1896fe559c7ce776524a
arps1989dev <arpita.sojitra@gmail.com>
2017-09-04 15:59:14 +0530

    add grid ti todo js

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Route, Redirect, Switch} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
 import asyncComponent from '../AsyncComponent';
 // import { isLoggedIn } from '../helper';
 
 const BeforeLoginLayout = asyncComponent(() => import ('../layout/BeforeLoginLayout'));
-const Login = asyncComponent(() => import ('../users/Login'));
+// const Login = asyncComponent(() => import ('../users/Login'));
 const Todo = asyncComponent(() => import('../Todo'));
 const routes = () => (
   <Switch>
